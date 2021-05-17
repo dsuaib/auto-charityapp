c5d1742a06d74f5383e1b0414e18d5df9ff837e6
Dani <dsuaib1@students.towson.edu>
2021-05-17 01:37:55 -0400

    Hiding Environment Variables

--- src/base.js
+++ src/base.js
@@ -2,21 +2,14 @@ import firebase from 'firebase/app'
 import"firebase/auth";
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyByD295W0KZ2eFAIxsQAJwAoZUGQNeiD8U",
-    authDomain: "ait715firstfirebase.firebaseapp.com",
-    projectId: "ait715firstfirebase",
-    storageBucket: "ait715firstfirebase.appspot.com",
-    messagingSenderId: "441226575806",
-    appId: "1:441226575806:web:4853218ae02940b7a403ef",
-    measurementId: "G-2V750XV68R"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGE,
+    messagingSenderId: process.env.REACT_APP_SENDER,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENT
   });
-
+  
 export default app;
 
-// process.env.REACT_APP_APIKEY
-// process.env.REACT_APP_DOMAIN
-// process.env.REACT_APP_DATABASE
-// process.env.REACT_APP_PROJECTID
-// process.env.REACT_APP_STORAGE
-// process.env.REACT_APP_SENDER
-// process.env.REACT_APP_MEASUREMENT
